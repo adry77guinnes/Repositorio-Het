708a7e31efe93ddfec07f8db38ac7b2fab40d65f
adry77guinnes <adry77guinnes@gmail.com>
2025-12-22 16:09:55 +0100

    Het2-Datos
    Subida Heterovaluable 2 Acceso a Datos

---
+++ heterovaluable2-accesodatos/src/main/java/com/example/heterovaluable2_accesodatos/Heterovaluable2AccesodatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.heterovaluable2_accesodatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heterovaluable2AccesodatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heterovaluable2AccesodatosApplication.class, args);
+	}
+
+}
